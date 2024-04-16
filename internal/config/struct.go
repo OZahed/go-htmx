9dfe5b0710cb5edcf330206eaf805ac154a73cab
OZahed <omidzahed8@gmail.com>
2024-04-16 17:52:30 +0330

    feat: struct config added

--- internal/config/struct.go
+++ internal/config/struct.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/url"
 	"os"
-	"reflect"
+	r "reflect"
 	"strconv"
 	"strings"
 	"time"
@@ -16,14 +16,18 @@ var (
 		time.Kitchen, time.RFC3339, time.RFC1123, time.RFC1123Z, time.ANSIC,
 		"2006/01/02", "2006/01/02 15:04:05", time.UnixDate, time.RubyDate}
 
-	stringSeparators = []string{",", ";", ";", "-"}
+	stringSeparators = []string{",", ";", ";", "-", " "}
+
+	timeType     = r.TypeOf(time.Time{})
+	durationType = r.TypeOf(time.Duration(0))
+	urlType      = r.TypeOf(&url.URL{})
 )
 
 var (
 	// later on you can read string value over a config server
 	// or read it from yaml or read value form json file
 	// just replace the Getter function with any other kind of
-	DefaultGetter ValueFunc = func(key, def string) string {
+	DefaultEnvGetter ValueFunc = func(key, def string) string {
 		val := os.Getenv(key)
 		if val == "" {
 			return def
@@ -51,7 +55,7 @@ type Marshaler struct {
 
 func NewMarshaler(k KeyFunc, g ValueFunc) *Marshaler {
 	if g == nil {
-		g = DefaultGetter
+		g = DefaultEnvGetter
 	}
 
 	if k == nil {
@@ -61,36 +65,25 @@ func NewMarshaler(k KeyFunc, g ValueFunc) *Marshaler {
 	return &Marshaler{BuildKey: k, Get: g}
 }
 
-func KeyFuncWithPrefix(prefix string) KeyFunc {
-	if !strings.HasSuffix(prefix, "_") {
-		prefix = prefix + "_"
-	}
-
-	return func(key string) string {
-		return prefix + key
-	}
-}
-
-func (m *Marshaler) Marshal(dest any, prefix string) error {
-	dst := reflect.ValueOf(dest)
-
-	return m.marshal(dst, prefix)
-}
-
 // TODO: append key names
 // TODO: Add Support for
 
 //nolint:funlen
-func (m *Marshaler) marshal(dst reflect.Value, prefix string) (err error) {
+func (m *Marshaler) Marshal(dest interface{}, prefix string) (err error) {
+	dst := r.ValueOf(dest)
 	valueType := dst.Type()
 
-	if valueType.Kind() != reflect.Pointer {
-		return errors.New("kind is not a pointer")
+	if valueType.Kind() != r.Pointer {
+		return fmt.Errorf("Kind %s is not a pointer", valueType.Kind())
+	}
+
+	if dst.IsNil() {
+		dst.Set(r.New(dst.Type().Elem()))
 	}
 
 	elm := valueType.Elem()
-	if elm.Kind() != reflect.Struct {
-		return errors.New("destination is not a struct")
+	if elm.Kind() != r.Struct {
+		return fmt.Errorf("destination is of type %s and not struct", elm.Kind())
 	}
 
 	valueType = valueType.Elem()
@@ -118,108 +111,111 @@ func (m *Marshaler) marshal(dst reflect.Value, prefix string) (err error) {
 		// KeyBuilder removes
 		strValues := m.Get(m.BuildKey(key), def)
 
-		if strValues == "" {
+		if strValues == "" && fieldType.Type.Kind() != r.Struct {
 			continue
 		}
 
-		err = m.marshalValue(fieldValue, strValues, prefix, key)
+		err = m.MarshalValue(fieldValue, strValues, prefix, key)
 		if err != nil {
 			return err
 		}
-
 	}
 
 	return nil
 }
 
-func (m *Marshaler) marshalValue(fieldValue reflect.Value, value, prefix, key string) error {
-	if !fieldValue.CanSet() {
+func (m *Marshaler) MarshalValue(reflectValue r.Value, strValue, prefix, key string) error {
+	if !reflectValue.CanSet() {
 		return nil
 	}
 
-	switch fieldValue.Kind() {
-	case reflect.String:
-		fieldValue.SetString(value)
-	case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int16, reflect.Int64:
-		n, err := strconv.ParseInt(value, 10, 64)
-		if err != nil {
-			return err
-		}
-		fieldValue.SetInt(n)
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		n, err := strconv.ParseUint(value, 10, 64)
-		if err != nil {
-			return err
-		}
-
-		fieldValue.SetUint(n)
-	case reflect.Float32, reflect.Float64:
-		f, err := strconv.ParseFloat(value, 64)
-		if err != nil {
-			return err
-		}
-		fieldValue.SetFloat(f)
-	case reflect.Map:
-		err := makeMap(value, fieldValue)
-		if err != nil {
-			return err
-		}
-
-	case reflect.Slice:
-		// TODO: parse Array
-		splits := splitStr(value)
-
-		if len(splits) > fieldValue.Len() {
-			fieldValue.Grow(len(splits) - fieldValue.Len())
-		}
-
-		fieldValue.SetLen(len(splits))
-
-		for i, split := range splits {
-			if !fieldValue.Index(i).CanAddr() {
-				continue
-			}
-
-			err := m.marshalValue(fieldValue.Index(i).Addr(), split, prefix, key)
-			if err != nil {
-				return err
-			}
-		}
-	case reflect.Struct:
-		if fieldValue.Type() == reflect.TypeOf(time.Time{}) {
-			t, err := parseTime(value)
-			if err != nil {
-				return err
-			}
-
-			fieldValue.Set(reflect.ValueOf(t))
-			return nil
-		}
-
-		// Url is always a pointer
-		if fieldValue.Type() == reflect.TypeOf(new(url.URL)) {
-			u, err := url.Parse(value)
-			if err != nil {
-				return err
-			}
-
-			fieldValue.Set(reflect.ValueOf(u))
-			return nil
-		}
-
-		err := m.marshal(fieldValue.Addr(), fmt.Sprintf("%s.%s", prefix, key))
-		if err != nil {
-			return err
-		}
+	if reflectValue.Kind() == r.Func {
+		return nil
 	}
 
-	if fieldValue.Type() == reflect.TypeOf(time.Duration(0)) {
-		d, err := time.ParseDuration(value)
+	// Checking for non-builtin types
+	switch reflectValue.Type() {
+	case timeType:
+		t, err := parseTime(strValue)
+		if err != nil {
+			return err
+		}
+		reflectValue.Set(r.ValueOf(t))
+		return nil
+	case urlType:
+		u, err := url.Parse(strValue)
 		if err != nil {
 			return err
 		}
 
-		fieldValue.Set(reflect.ValueOf(d))
+		reflectValue.Set(r.ValueOf(u))
+		return nil
+	case durationType:
+		d, err := time.ParseDuration(strValue)
+		if err != nil {
+			return err
+		}
+
+		reflectValue.Set(r.ValueOf(d))
+		return nil
+	}
+
+	// Checking for built int types
+	switch reflectValue.Kind() {
+	case r.String:
+		reflectValue.SetString(strValue)
+	case r.Int, r.Int8, r.Int32, r.Int16, r.Int64:
+		n, err := strconv.ParseInt(strValue, 10, 64)
+		if err != nil {
+			return err
+		}
+		reflectValue.SetInt(n)
+	case r.Uint, r.Uint8, r.Uint16, r.Uint32, r.Uint64, r.Uintptr:
+		n, err := strconv.ParseUint(strValue, 10, 64)
+		if err != nil {
+			return err
+		}
+
+		reflectValue.SetUint(n)
+	case r.Float32, r.Float64:
+		f, err := strconv.ParseFloat(strValue, 64)
+		if err != nil {
+			return err
+		}
+		reflectValue.SetFloat(f)
+	case r.Bool:
+		b, err := strconv.ParseBool(strValue)
+		if err != nil {
+			return err
+		}
+
+		reflectValue.SetBool(b)
+	case r.Map:
+		return m.parseMap(reflectValue, strValue)
+	case r.Slice:
+		return m.parseArray(strValue, reflectValue, key)
+	case r.Struct:
+		// The ParseEnv should be on pointer
+		ptr := reflectValue.Addr()
+		// checking for ParseEnv() error method first
+		parser := ptr.MethodByName("ParseEnv")
+		if parser.IsValid() {
+			callResult := parser.Call([]r.Value{r.ValueOf(key)})
+
+			e := callResult[0].Interface()
+
+			if e == nil {
+				return nil
+			}
+
+			return e.(error)
+		}
+
+		if !reflectValue.CanAddr() || reflectValue.Type() == r.TypeOf(struct{}{}) {
+			return nil
+		}
+
+		return m.Marshal(reflectValue.Addr().Interface(), key)
 	}
 
 	return nil
@@ -252,50 +248,60 @@ func parseStructTags(tagVal string) (key, def string) {
 // Only string and int are supported for now
 //
 // TODO: add map[string]any
-func makeMap(str string, value reflect.Value) error {
-	if value.Type().Kind() != reflect.Map {
+func (m *Marshaler) parseMap(value r.Value, str string) (err error) {
+	if value.Type().Kind() != r.Map {
 		return fmt.Errorf("%s is not a map", value.Type().Name())
 	}
 
+	fmt.Println(value.Len())
+
 	keyType := value.Type().Key()
 	valueType := value.Type().Elem()
-	value.Set(reflect.MakeMap(value.Type()))
-
-	if keyType.Kind() != reflect.String && keyType.Kind() != reflect.Int {
-		return fmt.Errorf("only int and string keys are acceptable, got key of type %s", keyType.Kind())
-	}
-
-	if valueType.Kind() != reflect.String && valueType.Kind() != reflect.Int {
-		return fmt.Errorf("only key and value data values are acceptable, got key of type %s", valueType.Kind())
-	}
+	value.Set(r.MakeMap(value.Type()))
 
 	kv := splitStr(str)
 	for _, pair := range kv {
 		splt := strings.Split(pair, ":")
+		fmt.Println(value.Len())
 		if len(splt) < 2 {
 			return fmt.Errorf("%s can not is in wrong format as key value pair", pair)
 		}
 
-		k := reflect.ValueOf(splt[0])
-		v := reflect.ValueOf(splt[1])
+		keyStr := strings.TrimSpace(splt[0])
+		valStr := strings.TrimSpace(splt[1])
+		k := r.New(keyType).Elem()
+		v := r.New(valueType).Elem()
+		fmt.Printf("key = %s value = %s\n", k.Kind().String(), v.Kind().String())
 
-		if keyType.Kind() == reflect.Int {
-			n, err := strconv.ParseInt(splt[1], 10, 64)
-			if err != nil {
-				return fmt.Errorf("key %s for map is can not be turned into int", splt[1])
-			}
-			k = reflect.ValueOf(n)
+		if err = m.MarshalValue(k, keyStr, "", ""); err != nil {
+			return fmt.Errorf("%s can not be parsed as %s", keyStr, k.Kind())
 		}
 
-		if valueType.Kind() == reflect.Int {
-			n, err := strconv.ParseInt(splt[1], 10, 64)
-			if err != nil {
-				return fmt.Errorf("value %s for map is can not be turned into int", splt[1])
-			}
-			v = reflect.ValueOf(n)
+		if err = m.MarshalValue(v, valStr, "", ""); err != nil {
+			return fmt.Errorf("%s can not be parsed as %s", valStr, v.Kind())
 		}
 
 		value.SetMapIndex(k, v)
+	}
+	return nil
+}
+
+func (m *Marshaler) parseArray(value string, fieldValue r.Value, currentKey string) error {
+	splits := splitStr(value)
+
+	if len(splits) > fieldValue.Len() {
+		fieldValue.Grow(len(splits) - fieldValue.Len())
+	}
+
+	fieldValue.SetLen(len(splits))
+
+	for i, split := range splits {
+		split = strings.TrimSpace(split)
+		// for slice values prefix should become key and there should be no keys
+		err := m.MarshalValue(fieldValue.Index(i), split, currentKey, "")
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
